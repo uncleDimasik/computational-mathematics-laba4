a759413c551b98d149d962ff24377029cea7e534
Dmitry Davydenko <dimasik1204dim@gmail.com>
2023-02-05 11:44:10 +0300

    done

---
+++ MathLab4-master/MathLab4-master/MLab4/Form1.cs
@@ -0,0 +1,71 @@
+﻿using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MLab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            var a = 0;
+            var b = Math.PI;
+            var sepsNumber = 4;
+            var eps = 0.001;
+            List<double> ys = new List<double>();
+            InitializeComponent();
+            Simpson s = new Simpson();
+            double f(double dx) => dx * Math.Sin(dx) * Math.Cos(dx);
+
+            while (true)
+            {
+                double res = s.Simpson_(f, a, b, sepsNumber);
+                double res1 = s.Simpson_(f, a, b, sepsNumber / 2);
+
+                if (s.isCorrectByRunge(res, res1, eps))
+                {
+                    label2.Text = res.ToString();
+                    label4.Text = sepsNumber.ToString();
+                    label6.Text = ((b - a) / sepsNumber).ToString();
+
+                    break;
+                }
+                sepsNumber += 1;
+
+            }
+            List<double> xs = new List<double>();
+            for (var k = 0; k <= sepsNumber; k++)
+            {
+                var h = (b - a) / sepsNumber;
+                var xk = a + k * h;
+                xs.Add(xk);
+            }
+    
+            for (int i = 0; i < xs.Count; i++)
+            {
+                ys.Add(xs[i] * Math.Sin(xs[i]) * Math.Cos(xs[i]));
+                formsPlot1.Plot.AddVerticalLine(xs[i], Color.Red, 1, ScottPlot.LineStyle.Solid);
+            }
+            formsPlot1.Plot.PlotScatter(xs.ToArray(), ys: ys.ToArray(), color: Color.Blue, markerSize: 5, label: "interpolation");
+
+            formsPlot1.Refresh();
+        }
+
+        private void formsPlot1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MathLab4-master/MathLab4-master/MLab4/Simpson.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MLab4
+{
+    class Simpson
+    {
+       public bool isCorrectByRunge(double Ihi, double Ih2i, double epsilon)
+        {
+            return (Math.Abs(Ihi - Ih2i) / 15d) < epsilon;
+        }
+
+        public double Simpson_(Func<double, double> f, double a, double b, int n)
+        {
+            var h = (b - a) / n;
+            var sum1 = 0d;
+            var sum2 = 0d;
+            for (var k = 1; k <= n; k++)
+            {
+                var xk = a + k * h;
+                if (k <= n - 1)
+                {
+                    sum1 += f(xk);
+                }
+
+                var xk_1 = a + (k - 1) * h;
+                sum2 += f((xk + xk_1) / 2);
+            }
+
+            var result = h / 3d * (1d / 2d * f(a) + sum1 + 2 * sum2 + 1d / 2d * f(b));
+            return result;
+        }
+    }
+}
